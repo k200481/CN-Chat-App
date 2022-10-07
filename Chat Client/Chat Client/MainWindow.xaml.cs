9885ead0d6a276553030193bfa1738c918d13b27
k200481 <81528611+k200481@users.noreply.github.com>
2022-10-08 00:21:56 +0500

    Add files via upload

---
+++ Chat Client/Chat Client/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Threading;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace Chat_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private Thread receiver;
+        public MainWindow()
+        {
+            // wpf stuff
+            InitializeComponent();
+            // initialize but not start the receiver thread
+            receiver = new Thread(RecvLoop);
+            receiver.IsBackground = true;
+            ipBox.Focus();
+        }
+
+        ~MainWindow()
+        {
+            if (server.Connected)
+            {
+                try
+                {
+                    if (receiver.IsAlive)
+                        receiver.Abort();
+                    server.Disconnect(false);
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void ConnectClickHandler(object sender, RoutedEventArgs e)
+        {
+            Connect();
+        }
+
+        private void ConnectKeydownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Connect();
+            }
+        }
+
+        private void Connect()
+        {
+            chatBox.Text = "";
+            string[] host_port = ipBox.Text.Split(':');
+            if (host_port.Length != 2)
+            {
+                chatBox.Text = "Format: <IP>:<Port>";
+                return;
+            }
+
+            try
+            {
+                Connect(host_port[0], host_port[1]);
+                msgBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                chatBox.Text = ex.Message;
+            }
+        }
+
+        private void Connect(string hostname, string port_in)
+        {
+            IPHostEntry he = Dns.GetHostEntry(hostname);
+            int port = Int32.Parse(port_in);
+
+            foreach (IPAddress ip in he.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetworkV6)
+                    continue;
+
+                IPEndPoint ipe = new IPEndPoint(ip, port);
+                server.Connect(ipe);
+
+                if (server.Connected)
+                {
+                    // gui stuff
+                    statusLabel.Content = "Connected";
+                    statusLabel.Foreground = new System.Windows.Media.SolidColorBrush(
+                        System.Windows.Media.Colors.Green
+                    );
+                    ipBox.IsEnabled = false;
+                    connectBtn.IsEnabled = false;
+                    // start receiving msgs
+                    receiver.Start();
+                    return;
+                }
+            }
+        }
+
+        private void SendClickHandler(object sender, RoutedEventArgs e)
+        {
+            SendMsg();
+        }
+
+        private void SendKeydownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMsg();
+            }
+        }
+
+        private void SendMsg()
+        {
+            try
+            {
+                server.Send(Encoding.ASCII.GetBytes(msgBox.Text.ToCharArray()));
+                chatBox.Text += "You: " + msgBox.Text + "\n";
+            }
+            catch (Exception ex)
+            {
+                chatBox.Text += ex.Message + "\n";
+                return;
+            }
+            msgBox.Text = "";
+        }
+
+        private void RecvLoop()
+        {
+            while (true)
+            {
+                if (server.Available > 0)
+                {
+                    RecvMsg();
+                }
+            }
+        }
+        private void RecvMsg()
+        {
+            try 
+            {
+                byte[] buf = new byte[server.Available];
+                server.Receive(buf);
+                this.Dispatcher.Invoke(() =>
+                {
+                    chatBox.Text += "Server: " + Encoding.ASCII.GetString(buf) + "\n";
+                }
+                );
+            }
+            catch (Exception e)
+            {
+                chatBox.Text += e.Message + "\n";
+            }
+        }
+
+        private void ScrollChangedHandler(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.ExtentHeightChange != 0)
+            {
+                chatBoxScroll.ScrollToVerticalOffset(chatBoxScroll.ExtentHeight);
+            }
+        }
+    }
+}
---
+++ Chat_Server/Chat_Server/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Threading;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace Chat_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thread listener;
+        private Thread receiver;
+        private Socket client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            receiver = new Thread(RecvMsgs);
+            receiver.IsBackground = true;
+            portBox.Focus();
+        }
+
+        ~MainWindow()
+        {
+            if (client != null && client.Connected)
+            {
+                try
+                {
+                    if(receiver.IsAlive)
+                        receiver.Abort();
+                    if (listener.IsAlive)
+                        listener.Abort();
+                    client.Disconnect(false);
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void ListenClickHandler(object sender, RoutedEventArgs e)
+        {
+            Listen();
+        }
+
+        private void ListenKeyHandler(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                Listen();
+            }
+        }
+
+        private void Listen()
+        {
+            statusLabel.Content = "Listening";
+            statusLabel.Foreground = new System.Windows.Media.SolidColorBrush(
+                System.Windows.Media.Colors.Yellow
+            );
+            
+            listener = new Thread(() => Listen(this.Dispatcher.Invoke(() => portBox.Text)));
+            listener.IsBackground = true;
+            listener.Start();
+
+            portBox.IsEnabled = false;
+            listenBtn.IsEnabled = false;
+            InputBox.Focus();
+        }
+
+        private void Listen(string port_in)
+        {
+            try
+            {
+                int port = Int32.Parse(port_in);
+                Socket skt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                skt.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+                skt.Listen(1);
+                client = skt.Accept();
+                receiver.Start();
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    statusLabel.Content = "Connected";
+                    statusLabel.Foreground = new System.Windows.Media.SolidColorBrush(
+                        System.Windows.Media.Colors.Green
+                    );
+                });
+            }
+            catch (Exception e)
+            {
+                this.Dispatcher.Invoke(() => {
+                    chatBox.Text += e.Message + "\n";
+                    listenBtn.IsEnabled = true;
+                    portBox.IsEnabled = true;
+                    statusLabel.Content = "Not Connected";
+                    statusLabel.Foreground = new System.Windows.Media.SolidColorBrush(
+                        System.Windows.Media.Colors.Red
+                    );
+                    portBox.Focus();
+                });
+            }
+        }
+
+        private void SendClickHandler(object sender, RoutedEventArgs e)
+        {
+            SendMsg();
+        }
+
+        private void SendKeyHandler(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                SendMsg();
+            }
+        }
+
+        private void SendMsg()
+        {
+            try
+            {
+                if (client == null)
+                    throw new Exception("Not connected");
+
+                client.Send(Encoding.ASCII.GetBytes(InputBox.Text.ToCharArray()));
+                chatBox.Text += "You: " + InputBox.Text + "\n";
+            }
+            catch (Exception ex)
+            {
+                chatBox.Text += ex.Message + "\n";
+            }
+            InputBox.Text = "";
+        }
+
+        private void RecvMsgs()
+        {
+            while (true)
+            {
+                if(client != null && client.Available > 0)
+                {
+                    byte[] buf = new byte[client.Available];
+                    client.Receive(buf);
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        chatBox.Text += "Client: " + Encoding.ASCII.GetString(buf) + "\n";
+                    });
+                }
+            }
+        }
+
+        private void ScrollChangedHandler(object sender, ScrollChangedEventArgs e)
+        {
+            if(e.ExtentHeightChange != 0)
+            {
+                chatBoxScroll.ScrollToVerticalOffset(chatBoxScroll.ExtentHeight);
+            }
+        }
+    }
+}
